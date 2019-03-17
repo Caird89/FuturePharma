6e8cb4dcdba3c59197278cb3bf238fd7dfab8e9d
Roman <Roman200333@hotmail.ca>
2019-03-17 15:34:52 -0400

    Implemented Basic Question Display
    Added the ability to show questions per fragment tab  by using an array to store them.

--- app/src/main/java/wellcairdfor/futurepharma/MainActivity.java
+++ app/src/main/java/wellcairdfor/futurepharma/MainActivity.java
@@ -1,7 +1,9 @@
 package wellcairdfor.futurepharma;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gogogo();
+    }
+
+
+    public void gogogo()
+    {
+        Intent intent = new Intent(this, FiveQuestions.class);
+        startActivity(intent);
+
     }
 }
